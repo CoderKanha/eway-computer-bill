4cef1dd137cf974fbaa39d7ac0c1ef99258c8496
Kanhiya <hkanhiya123@gmail.com>
2021-11-16 23:15:36 +0530

    setup for bill view for print

--- src/app/e-way-bill/e-way-bill-routing.module.ts
+++ src/app/e-way-bill/e-way-bill-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateEwayBillComponent } from './create-eway-bill/create-eway-bill.component';
+import { ViewEwayBillComponent } from './view-eway-bill/view-eway-bill.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CreateEwayBillComponent
+  },
+  {
+    path: 'view-bill',
+    component: ViewEwayBillComponent
   }
 ];
 
---
+++ src/app/e-way-bill/view-eway-bill/view-eway-bill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-eway-bill',
+  templateUrl: './view-eway-bill.component.html',
+  styleUrls: ['./view-eway-bill.component.scss']
+})
+export class ViewEwayBillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
